aebe1247766b2425cae47684e62f5bb2ef5a53ae
Simon Harrer <simon.harrer@gmail.com>
2015-12-14 12:18:41 +0100

    More GUI tests and fixed EDT Thread issues on the way.

--- src/test/java/net/sf/jabref/gui/GUITest.java
+++ src/test/java/net/sf/jabref/gui/GUITest.java
@@ -1,17 +1,18 @@
 package net.sf.jabref.gui;
 
 import net.sf.jabref.JabRefMain;
+import net.sf.jabref.gui.preftabs.PreferencesDialog;
 import org.assertj.swing.core.GenericTypeMatcher;
-import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.dependency.jsr305.Nonnull;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.swing.*;
 
-import static org.assertj.swing.launcher.ApplicationLauncher.application;
+import static org.assertj.swing.finder.WindowFinder.findDialog;
 import static org.assertj.swing.finder.WindowFinder.findFrame;
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
 
 public class GUITest extends AssertJSwingJUnitTestCase {
 
@@ -33,8 +34,42 @@ public class GUITest extends AssertJSwingJUnitTestCase {
     @Test
     public void testNewFile() {
         FrameFixture mainFrame = findFrame(JabRefFrame.class).using(robot());
-        mainFrame.menuItemWithPath("File", "New database").click();
+        newDatabase(mainFrame);
         mainFrame.menuItemWithPath("File", "Close database").click();
+        exitJabRef(mainFrame);
+    }
+
+    private void newDatabase(FrameFixture mainFrame) {
+        mainFrame.menuItemWithPath("File", "New database").click();
+    }
+
+    @Test
+    public void testCreateBibtexEntry() {
+        FrameFixture mainFrame = findFrame(JabRefFrame.class).using(robot());
+
+        newDatabase(mainFrame);
+
+        mainFrame.menuItemWithPath("BibTeX", "New entry").click();
+        findDialog(EntryTypeDialog.class).using(robot()).button(new GenericTypeMatcher<JButton>(JButton.class) {
+            @Override protected boolean isMatching(@Nonnull JButton jButton) {
+                return "Book".equals(jButton.getText());
+            }
+        }).click();
+
+        exitJabRef(mainFrame);
+    }
+
+    @Test
+    public void testOpenAndSavePreferences() {
+        FrameFixture mainFrame = findFrame(JabRefFrame.class).using(robot());
+
+        mainFrame.menuItemWithPath("Options", "Preferences").click();
+        findDialog(PreferencesDialog.class).using(robot()).button(new GenericTypeMatcher<JButton>(JButton.class) {
+            @Override protected boolean isMatching(@Nonnull JButton jButton) {
+                return "OK".equals(jButton.getText());
+            }
+        }).click();
+
         exitJabRef(mainFrame);
     }
 
